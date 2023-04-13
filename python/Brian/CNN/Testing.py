6cdc309f328c6d85fbb5a223bcf7b4f63d978432
bwei551crazy <crazykwaibao@gmail.com>
2023-04-13 17:41:55 +1200

    Started the Loading gui
    Started incorporating a gui that shows the progress bar while its training.

--- python/Brian/CNN/Testing.py
+++ python/Brian/CNN/Testing.py
@@ -1,13 +1,17 @@
+import torch
+import numpy as np
+from CNN_model import CNN, nn
+from GUI_loading import QApplication, MyApp, sys, self 
+from dataset import userData
+import torchvision.transforms as transforms
+from torch.utils.data import DataLoader
 
-import torch.nn as nn
-from tqdm import tqdm #for progress bar on console
-
-""" batch_size = 50
+batch_size = 50
 num_classes = 26
 learning_rate = 0.001
 num_epochs = 20
 
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu') 
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 train_dataset = userData('C:\\Users\\OEM\\Downloads\\archive\sign_mnist_train\\sign_mnist_train.csv',
                          transform=transforms.Compose([transforms.ToTensor(),
@@ -20,52 +24,18 @@ test_dataset = userData('C:\\Users\\OEM\\Downloads\\archive\sign_mnist_test\\sig
                                                         transforms.Normalize(mean = (0.1306,), std = (0.3082,))]))
 
 train_loader = DataLoader(dataset = train_dataset, batch_size = batch_size, shuffle = True)
-test_loader = DataLoader(dataset = test_dataset, batch_size = batch_size, shuffle = False) """
+test_loader = DataLoader(dataset = test_dataset, batch_size = batch_size, shuffle = False) 
 
-class CNN(nn.Module):
-
-    #input channels = the colour of image. 3 for colour, 1 for greyscale
-    #out channels = number of features. Seems like this out can be any number u choose. 
-    def __init__(self, num_classes):
-        super(CNN, self).__init__()
-        #oiriginal image is 32 x 32
-        self.c1 = nn.Conv2d(in_channels = 1, out_channels = 32, kernel_size = 3) #image becomes 30 x 30
-        self.c2 = nn.Conv2d(in_channels= 32, out_channels = 32, kernel_size = 3) #image becomes 28 x 28 but with 32 channels???
-        self.max_pool1 = nn.MaxPool2d(kernel_size = 2, stride = 2) #image 28 x 28
-        #kernal_size = a n x m amount of pixels used to create a pixel for the filtered image. Basically the amount use to filer an image
-        #Stride = amount of pixels moved to the right from the starting position of the kernal
-
-        self.c3 = nn.Conv2d(in_channels = 32, out_channels = 64, kernel_size = 3) # image becomes 26 x 26
-        self.c4 = nn.Conv2d(in_channels= 64, out_channels= 64, kernel_size = 3) #image becomes 24 x 24
-        self.max_pool2 = nn.MaxPool2d(kernel_size = 2, stride = 2) # image becomes 24 x 24
-        
-        self.fc1 = nn.Linear(1600, 128) #first value seems to be final number of channels * final image size
-        self.relu1 = nn.ReLU()
-        self.fc2 = nn.Linear(128, num_classes)
-
-    def forward(self, x):
-        out = self.c1(x)
-        out = self.c2(out)
-        out = self.max_pool1(out)
-
-        out = self.c3(out)
-        out = self.c4(out)
-        out = self.max_pool2(out)
-
-        out = out.reshape(out.size(0), -1)
-
-        out = self.fc1(out)
-        out = self.relu1(out)
-        out = self.fc2(out)
-        return out
-    
-"""model = CNN(num_classes)
+model = CNN(num_classes)
 
 criterion = nn.CrossEntropyLoss()
 
 optimizer = torch.optim.SGD(model.parameters(), lr = learning_rate, weight_decay = 0.005, momentum = 0.9)
 
 total_step = len(train_loader)
+
+app = QApplication(sys.argv)
+ex = MyApp()
 
 #Saving and loading of model. The print function within for loop (lines 81 and 82) to see the tensors within the trained model
 #FILE = "modelV1.pth"
@@ -74,8 +44,9 @@ total_step = len(train_loader)
 #loaded_model.load_state_dict(torch.load(FILE))
 #loaded_model.eval()
 
-for param in model.parameters():
-    print(param)
+#for param in model.parameters():
+#    print(param)
+
 #====Apparently lazy method of saving models===
 #FILE = "modelV1.pth"
 #torch.save(model, FILE)
@@ -98,8 +69,10 @@ for epoch in range(num_epochs):
         optimizer.zero_grad()
         loss.backward()
         optimizer.step()
+        self.step += 1
 
     print('Epoch [{}/{}], Loss: {:.4f}'.format(epoch+1, num_epochs, loss.item()))
+
 
 with torch.no_grad():
     correct = 0
@@ -115,4 +88,6 @@ with torch.no_grad():
         total += labels.size(0)
         correct += (predicted == labels).sum().item()
 
-    print('Accuracy of the network on the {} train images: {} %'.format(27455, 100*correct/total)) """
+    print('Accuracy of the network on the {} train images: {} %'.format(27455, 100*correct/total))
+
+sys.exit(app.exec_())

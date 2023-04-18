b9c2510c7dcbcfdf9c055fe541bbb53ae589797b
bwei551crazy <crazykwaibao@gmail.com>
2023-04-18 22:47:31 +1200

    found the solution for the program bar loading
    Added QApplication.processEvents()

--- python/Brian/CNN/GUI_loading.py
+++ python/Brian/CNN/GUI_loading.py
@@ -2,23 +2,8 @@ import sys
 from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QProgressBar
 from PyQt5.QtCore import QThread, pyqtSignal
 import time
-from tqdm import tqdm
-#from CNN_model import initParam
-#from CNN_model import num_epochs
-
-class Thread(QThread):
-    _signal = pyqtSignal(int)
-    def __init__(self, epoch):
-        super(Thread, self).__init__()
-        self.epoch = epoch
-
-    def __del__(self):
-        self.wait()
-
-    def run(self):
-        for i in range(self.epoch):
-            time.sleep(0.1)
-            self._signal.emit(i)
+#from tqdm import tqdm
+#from Testing import Test_Train
 
 class MyApp(QWidget):
 
@@ -39,18 +24,14 @@ class MyApp(QWidget):
         self.pbar.setValue(self.step)           # update the progress bar
         self.show()
 
-    def startThread(self, epochVal):
-        self.thread = Thread(100)
-        self.thread._signal.connect(self.updateProgress) #connects the signal in run function with the updateProgress method in the form of parameter
-        self.thread.start()
-
     def updateProgress(self, progress):
         self.pbar.setValue(progress)
+        QApplication.processEvents()
 
     
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     ex = MyApp()
-    ex.startThread(100)
+    #ex.startThread(100)
     ex.show()
     sys.exit(app.exec_())

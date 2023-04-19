325dd008ad46dafd0a47ffc7638044efbf38796e
bwei551crazy <crazykwaibao@gmail.com>
2023-04-20 01:08:04 +1200

    (Test) Saving and loading up model
    Added saving model
    Added Loading model (test)

--- python/Brian/CNN/Training.py
+++ python/Brian/CNN/Training.py
@@ -10,6 +10,7 @@ from PyQt5.QtWidgets import QApplication
 import sys
 #from Isaac.PyGUI.AlexNet import AlexNet
 from LeNet5Model import LeNet5
+from PIL import Image
 #from python.Shaaran.PyQtGUI.progressBar import PBar
 
 class Test_Train:
@@ -50,7 +51,8 @@ class Test_Train:
         criterion = nn.CrossEntropyLoss()
         optimizer = torch.optim.SGD(model.parameters(), lr = self.learning_rate, weight_decay = 0.005, momentum = 0.9)
 
-        #for param in model.parameters():
+        #used for debugging
+        # for param in model.parameters():
             #print(param.size())
 
         #Training along with accuracy of model
@@ -93,11 +95,43 @@ class Test_Train:
             #Displays this onto the textbox that is located above the progress bar
             self.progressBar.tb.append('Epoch [{}/{}], Loss: {:.4f}'.format(epoch+1, self.num_epochs, loss.item()))
             self.progressBar.tb.append('Accuracy of the network on the {} train images: {:.2f} % \n'.format(27455, 100*correct/total))
+        return model
+
+    def saving_model(self, trained_model, file_name):
+        #if the method does return something, will save the model
+        if trained_model is not None:
+            torch.save(trained_model.state_dict(), file_name )
+    
+    def load_model(self, filename, modelType, device):
+        if modelType == 'CNN':
+            loaded_model = CNN(self.num_classes).to(device)
+        elif modelType == 'LeNet5':
+            loaded_model = LeNet5(self.num_classes).to(device)
+        #else:
+        #    loaded_model = AlexNet(self.num_classes).to(device)
+
+        loaded_model.load_state_dict(torch.load(filename))
+        return loaded_model
+        #loaded_model.eval()
+
           
 #For testing purposes when running on this file
 if __name__ == '__main__':
-    app = QApplication(sys.argv)
+    #app = QApplication(sys.argv)
     stupid = Test_Train(50, 26, 0.001, 20)
     device, train_dataset, test_dataset = stupid.setting_up('C:\\Users\\OEM\\Downloads\\archive\\sign_mnist_train\\sign_mnist_train.csv', 'C:\\Users\\OEM\\Downloads\\archive\\sign_mnist_test\\sign_mnist_test.csv', )
-    train_load, test_load = stupid.loading_up(train_dataset, test_dataset)
-    stupid.runModel(train_load, test_load, device, "CNN")
+    #train_load, test_load = stupid.loading_up(train_dataset, test_dataset)
+    #model = stupid.runModel(train_load, test_load, device, "CNN")
+    #filename = "model1"
+    #stupid.saving_model(model, filename + ".pth")
+    loaded_model = stupid.load_model("model1.pth", "CNN", device)
+    loaded_model.eval()
+    #input_image = Image.open(image_file_name)
+    #with torch.no_grad():
+
+
+ #All stuff underneath is just the required file directory for testing on different devices. 
+    #'C:\\Users\\brian\Documents\\project-1-python-team_16\\dataset\\sign_mnist_train.csv'
+    #'C:\\Users\\brian\Documents\\project-1-python-team_16\\dataset\\sign_mnist_test.csv'
+    #'C:\\Users\\OEM\\Downloads\\archive\sign_mnist_train\\sign_mnist_train.csv'
+    #'C:\\Users\\OEM\\Downloads\\archive\sign_mnist_test\\sign_mnist_test.csv')

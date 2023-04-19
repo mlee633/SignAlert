e648a1b6bfd13b68d31676f415160b1376ecf749
bwei551crazy <crazykwaibao@gmail.com>
2023-04-19 23:14:11 +1200

    Did the progress bar with button
    Added in a working progress bar with a stop button that will stop the training progress.

--- python/Brian/CNN/GUI_loading.py
+++ python/Brian/CNN/GUI_loading.py
@@ -1,5 +1,5 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QProgressBar
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QProgressBar, QVBoxLayout
 from PyQt5.QtCore import QThread, pyqtSignal
 import time
 #from tqdm import tqdm
@@ -14,19 +14,30 @@ class MyApp(QWidget):
     def initUI(self):
         #setting up the dimensions of progress bar
         self.pbar = QProgressBar(self)
-        self.pbar.setGeometry(30, 40, 200, 25)
+        self.action = True # for continue training
+        stop_training_button = QPushButton('Stop')
+        stop_training_button.setGeometry(550, 450, 20, 20)
+        stop_training_button.clicked.connect(self.button_clicked)
+        self.pbar.setGeometry(360, 400, 25, 25)
 
         self.step = 0                         # init timer as 0
 
-        self.setWindowTitle('QProgressBar')
+        self.setWindowTitle('Training in Progress')
         self.setGeometry(300, 300, 300, 200)
 
         self.pbar.setValue(self.step)           # update the progress bar
+        vbox = QVBoxLayout()
+        vbox.addWidget(self.pbar)
+        vbox.addWidget(stop_training_button)
+        self.setLayout(vbox)
         self.show()
 
     def updateProgress(self, progress):
         self.pbar.setValue(progress)
         QApplication.processEvents()
+    
+    def button_clicked(self):
+        self.action = False
 
     
 if __name__ == '__main__':
--- python/Brian/CNN/Training.py
+++ python/Brian/CNN/Training.py
@@ -8,9 +8,9 @@ from CNN_model import CNN
 from GUI_loading import MyApp
 from PyQt5.QtWidgets import QApplication
 import sys
-from Isaac.PyGUI.AlexNet import AlexNet
-from Shaaran.LeNetModel import LeNet5
-from Shaaran.PyQtGUI.progressBar import PBar
+#from Isaac.PyGUI.AlexNet import AlexNet
+from LeNet5Model import LeNet5
+#from python.Shaaran.PyQtGUI.progressBar import PBar
 
 class Test_Train:
     def __init__(self, batch_size, num_classes, learning_rate, num_epochs):
@@ -18,7 +18,7 @@ class Test_Train:
         self.num_classes = num_classes
         self.learning_rate = learning_rate
         self.num_epochs = num_epochs
-        self.progressBar = PBar()
+        self.progressBar = MyApp() #PBar()
 
 
     def setting_up(self, file_location_train, file_location_test):
@@ -32,49 +32,55 @@ class Test_Train:
                                                         transforms.Resize((32,32)),
                                                         transforms.Normalize(mean = (0.1306,), std = (0.3082,))]))
         return device, train_dataset, test_dataset 
+    
     def loading_up(self, train_dataset, test_dataset):
         train_loader = DataLoader(dataset = train_dataset, batch_size = self.batch_size, shuffle = True)
         test_loader = DataLoader(dataset = test_dataset, batch_size = self.batch_size, shuffle = False) 
     
         return train_loader, test_loader
+    
     def runModel(self, train_loader, test_loader, device, modelType):
         #Remember to add in if statements to allow users to change models here. 
         if modelType == 'CNN':
             model = CNN(self.num_classes).to(device)
         elif modelType == 'LeNet5':
             model = LeNet5(self.num_classes).to(device)
-        else:
-            model = AlexNet(self.num_classes).to(device)
+        #else:
+        #    model = AlexNet(self.num_classes).to(device)
 
 
         criterion = nn.CrossEntropyLoss()
 
         optimizer = torch.optim.SGD(model.parameters(), lr = self.learning_rate, weight_decay = 0.005, momentum = 0.9)
 
-        
         #for param in model.parameters():
             #print(param.size())
         counter = 0
-
         for epoch in range(self.num_epochs):
             for i, (images, labels) in enumerate(train_loader): #tqdm(enumerate(train_loader), total = len(train_loader), leave = False):
-                labels = labels.T
-                labels = np.ravel(labels)
-                labels = torch.from_numpy(labels)
-                images = images.to(device)
-                labels = labels.to(device)
+                if self.progressBar.action == True:
+                    labels = labels.T
+                    labels = np.ravel(labels)
+                    labels = torch.from_numpy(labels)
+                    images = images.to(device)
+                    labels = labels.to(device)
 
-                outputs = model(images) 
-                loss = criterion(outputs, labels)
+                    outputs = model(images) 
+                    loss = criterion(outputs, labels)
 
-                optimizer.zero_grad()
-                loss.backward()
-                optimizer.step()
-                counter += 1
-                self.progressBar.changeValue(int(counter / (self.num_epochs * self.batch_size)))
-                
-
+                    optimizer.zero_grad()
+                    loss.backward()
+                    optimizer.step()
+                    counter += 1
+                    self.progressBar.updateProgress(int(100 *counter / (self.num_epochs * len(train_loader))))
+                else:
+                    return
+                    #break
+            #if self.progressBar.action == False:
+            #    break
+            #else:
             print('Epoch [{}/{}], Loss: {:.4f}'.format(epoch+1, self.num_epochs, loss.item()))
+        
 
         with torch.no_grad():
             correct = 0
@@ -94,6 +100,8 @@ class Test_Train:
 
 
 if __name__ == '__main__':
+    app = QApplication(sys.argv)
     stupid = Test_Train(50, 26, 0.001, 20)
-    device, train_load, test_load = stupid.setting_up('C:\\Users\\OEM\\Downloads\\archive\sign_mnist_train\\sign_mnist_train.csv', 'C:\\Users\\OEM\\Downloads\\archive\sign_mnist_test\\sign_mnist_test.csv', )
-    stupid.runModel(train_load, test_load, device)
+    device, train_dataset, test_dataset = stupid.setting_up('C:\\Users\\OEM\\Downloads\\archive\\sign_mnist_train\\sign_mnist_train.csv', 'C:\\Users\\OEM\\Downloads\\archive\\sign_mnist_test\\sign_mnist_test.csv', )
+    train_load, test_load = stupid.loading_up(train_dataset, test_dataset)
+    stupid.runModel(train_load, test_load, device, "CNN")

2c6d46f1db9a19ab62cdecefcb34df412653ea1a
sela847 <sela847@aucklanduni.ac.nz>
2023-04-21 22:07:03 +1200

    Big changes to the TestingGui, should all work now

--- python/Main/Testing_modelGUI.py
+++ python/Main/Testing_modelGUI.py
@@ -18,7 +18,7 @@ class TestingModelGUI(QMainWindow):
         self.Exit_button = QPushButton("Exit")
         self.Exit_button.clicked.connect(self.exit_application)
         self.statusBar().addPermanentWidget(self.Exit_button)
-        self.choose_images_button.clicked.connect(self.show_image_options)
+        
 
 
     def initUI(self):
@@ -28,11 +28,11 @@ class TestingModelGUI(QMainWindow):
 
         # Create the "Choose Model" button
         self.choose_model_button = QPushButton("Choose Trained Model", self)
-        self.choose_model_button.clicked.connect(self.show_model_options)
+        self.choose_model_button.clicked.connect(self.ModelbuttonClick)
 
         # Create the "Choose Images" button
         self.choose_images_button = QPushButton("Choose Images", self)
-
+        self.choose_images_button.clicked.connect(self.ImagebuttonClick)
         # Create the "Open Webcam" button
         self.choose_webcam_button = QPushButton("Open Webcam", self)
         self.choose_webcam_button.clicked.connect(self.open_camera_window)
@@ -60,47 +60,47 @@ class TestingModelGUI(QMainWindow):
         # Keep a reference to the tab widget
         self.tab_widget = None
     def openTesting(self):
-        model = torch.load(self.nameFile)
-        test = Testing.TestResults(model=model, images=self.images)
-        test.show()
-    def show_model_options(self):
-        # Create a new tab widget
-        self.tab_widget = QTabWidget()
+        model = torch.load(str(self.nameFile))
+        self.test = Testing.TestResults(model=model, images=self.images)
+        self.test.show()
+    # def show_model_options(self):
+    #     # Create a new tab widget
+    #     self.tab_widget = QTabWidget()
 
-        # Create a tab for choosing the model file
-        file_tab = QWidget()
-        file_layout = QVBoxLayout()
-        file_label = QLabel("Choose model file:")
-        file_layout.addWidget(file_label)
-        file_button = QPushButton("Open", self)
-        file_button.clicked.connect(self.ModelbuttonClick)
-        file_layout.addWidget(file_button)
-        file_tab.setLayout(file_layout)
-        self.tab_widget.addTab(file_tab, "Model")
+    #     # Create a tab for choosing the model file
+    #     file_tab = QWidget()
+    #     file_layout = QVBoxLayout()
+    #     file_label = QLabel("Choose model file:")
+    #     file_layout.addWidget(file_label)
+    #     file_button = QPushButton("Open", self)
+    #     file_button.clicked.connect(self.ModelbuttonClick)
+    #     file_layout.addWidget(file_button)
+    #     file_tab.setLayout(file_layout)
+    #     self.tab_widget.addTab(file_tab, "Model")
 
         # Set the tab widget as the central widget
         self.setCentralWidget(self.tab_widget)
 
     def ModelbuttonClick(self):
         self.nameFile= QFileDialog.getOpenFileName(self,"Open training dataset",r"<Default dir>", "PTH (*.pth);;All Files (*)")[0]
+        print(self.nameFile)
+    # def show_image_options(self):
+    #     # Create a new tab widget
+    #     self.tab_widget = QTabWidget()
 
-    def show_image_options(self):
-        # Create a new tab widget
-        self.tab_widget = QTabWidget()
+    #     # Create a tab for choosing the image files
+    #     file_tab = QWidget()
+    #     file_layout = QVBoxLayout()
+    #     file_label = QLabel("Choose image files:")
+    #     file_layout.addWidget(file_label)
+    #     file_button = QPushButton("Open", self)
+    #     file_button.clicked.connect(self.ImagebuttonClick)
+    #     file_layout.addWidget(file_button)
+    #     file_tab.setLayout(file_layout)
+    #     self.tab_widget.addTab(file_tab, "Images")
 
-        # Create a tab for choosing the image files
-        file_tab = QWidget()
-        file_layout = QVBoxLayout()
-        file_label = QLabel("Choose image files:")
-        file_layout.addWidget(file_label)
-        file_button = QPushButton("Open", self)
-        file_button.clicked.connect(self.ImagebuttonClick)
-        file_layout.addWidget(file_button)
-        file_tab.setLayout(file_layout)
-        self.tab_widget.addTab(file_tab, "Images")
-
-        # Set the tab widget as the central widget
-        self.setCentralWidget(self.tab_widget)
+    #     # Set the tab widget as the central widget
+    #     self.setCentralWidget(self.tab_widget)
 
     def ImagebuttonClick(self):
         self.images= QFileDialog.getOpenFileNames(self,"Open Image Files",r"<Default dir>", "Images (*.png *.jpg *.jpeg *.bmp);;All Files (*)")[0]
@@ -118,7 +118,7 @@ class TestingModelGUI(QMainWindow):
         self.initUI()
 
     def exit_application(self):
-        self.destroy()
+        self.close()
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
